96c709257c9d419e9580f908cf792b31c1301dd1
MD. Zakirul Islam <rifat40520@gmail.com>
2025-07-20 10:38:03 +0600

    Add files via upload

---
+++ Mid/Form.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<title>This is AIUB Registration Form</title>
+<body>
+
+<center>
+<h1 style="color: blue;"> AIUB </h1>
+<h3 style="color: blue;"> Registration Form </h3>
+</center>
+<h3>Complete the Registration</h3>
+<table>
+<tr>
+    <td>Full Name:</td>
+    <td><input type="text"></td>
+</tr>
+
+<tr>
+    <td>Email:</td>
+    <td><input type="text"></td>
+</tr>
+
+<tr>
+    <td>Password:</td>
+    <td><input type="text"></td>
+</tr>
+
+<tr>
+    <td>Gender:</td>
+    <td><input type="radio" name="des">Male 
+    <input type="radio" name="des">Other </td>
+</tr>
+
+<tr>
+    <td>Languages Known:</td>
+    <td><input type="checkbox">English
+    <input type="checkbox">Bangla
+    <input type="checkbox">Hindi</td>
+</tr>
+
+<tr>
+    <td>Country:</td>
+    <td><select name="Country">
+    <option value=" ">--select--</option>
+    <option value="Bangladesh">Bangladesh</option>
+    <option value="India">India</option>
+    <option value="Pakisthan">Pakisthan</option>
+    <option value="America">America</option>
+    </select></td>
+    
+</tr>
+
+<tr>
+<td>Date of Birth:</td>
+<td><input type="date"></td>
+</tr>
+
+<tr>
+<td>Upload Photo:</td>
+<td><input type="file"></td>
+</tr>
+
+<tr>
+<td>Comments:</td>
+<td><input type="text"></td>
+</tr>
+<tr>
+    <td> </td>
+   <td><input type="button" value="Register"></td> 
+</tr>
+
+</table>
+
+</body>
+</html>
